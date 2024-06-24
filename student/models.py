812071d49f940b1b7233a92227f976442b5723a5
Zippy-Waluse <zippywaluse@gmail.com>
2024-06-24 11:07:29 +0300

    made changes

--- student/models.py
+++ student/models.py
@@ -2,37 +2,39 @@ from django.db import models
 
 # Create your models here.
 
-class Student(models.Model):
-    first_name=models.CharField(max_length=20)
-    last_name=models.CharField(max_length=20)
-    email=models.EmailField()
-    code=models.PositiveSmallIntegerField()
-    date_of_birth=models.DateField()
-    country=models.CharField(max_length=20)
-    bio=models.TextField()
-    Age=models.SmallIntegerField()
-    
+class Class(models.Model):
+    clas_id = models.SmallIntegerField()
+    class_name = models.CharField(max_length=20)
+    course = models.CharField(max_length=20)
+    teacher = models.CharField(max_length=20)
+    enrollnment = models.SmallIntegerField()
+    room_number = models.SmallIntegerField()
+    class_duration = models.CharField(max_length=20)
+    meeting_days = models.CharField(max_length=20)
+    class_rep = models.CharField(max_length=20)
+    class_capacity = models.SmallIntegerField()
 
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-    
-# class Course(models.Model):
-#     course_name = models.CharField(max_length=100)
-#     course_code = models.CharField(max_length=20, unique=True)
-#     Subject = models.TextField()
-#     department = models.CharField(max_length=50)
-#     updated_at = models.DateTimeField(auto_now=True)
-    
-#     def __str__(self):
-#         return f"{self.course_name} ({self.course_code})"
+class Course(models.Model):
+    course_id = models.SmallIntegerField()
+    course_name = models.CharField(max_length=20)
+    course_description = models.TextField()
+    department = models.CharField(max_length=20)
+    course_instructor = models.CharField(max_length=20)
+    assessment_requirements = models.TextField()
+    course_fee = models.IntegerField()
 
 class Teacher(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField()
-    department = models.CharField(max_length=50)
-    phone_number = models.CharField(max_length=15)
-
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    gender = models.CharField(max_length=20)
+    email = models.EmailField()
+    department = models.CharField(max_length=20)
+    teacher_id = models.SmallIntegerField()
+    date_of_joining = models.DateField()
+    nationality = models.CharField(max_length=20)
+    bio = models.TextField()
+    years_of_experience = models.SmallIntegerField()
+    photo = models.ImageField()
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        
+        return f"{self.firstname} {self.lastname}"
